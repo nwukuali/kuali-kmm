d7d907c6c2dcd8b0907a8ddd35765bb4c1e65b99
bobjana <bobjana@gmail.com>
2012-11-28 07:53:03 +0200

    KFSNWU-52: Resolve property / configuration startup issues * bootstrap configuration changes * web xml package changes * static (temp) configuration.xml for dev purposes

--- src/java/org/kuali/ext/mm/common/sys/context/WebApplicationInitListener.java
+++ src/java/org/kuali/ext/mm/common/sys/context/WebApplicationInitListener.java
@@ -16,19 +16,10 @@
 package org.kuali.ext.mm.common.sys.context;
 
 import org.apache.log4j.Logger;
-import org.kuali.ext.mm.util.MMDecimal;
-import org.kuali.ext.mm.web.format.MMDecimalFormatter;
-import org.kuali.rice.core.api.config.property.ConfigContext;
-import org.kuali.rice.core.framework.config.property.SimpleConfig;
-import org.kuali.rice.core.web.format.Formatter;
 import org.kuali.rice.core.web.listener.KualiInitializeListener;
-import org.springframework.util.Log4jConfigurer;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import java.io.IOException;
-import java.util.Enumeration;
-import java.util.Properties;
 
 
 @SuppressWarnings("unchecked")
@@ -44,41 +35,55 @@ public class WebApplicationInitListener extends KualiInitializeListener /*extend
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-			//TODO: Expose Constants attribute on servlet context if required....
+			//TODO: NWU - Expose Constants attribute on servlet context if required....
 //        sce.getServletContext().setAttribute("Constants", new JSTLConstants(KEWConstants.class));
-        SimpleConfig config = new SimpleConfig("classpath:" + ContextConstants.CONFIGURATION_LOCATIONS_FILE);
-        try {
-            config.parseConfig();
-        }
-        catch (IOException e1) {
-            e1.printStackTrace();
-        }
-        System.setProperty("spring.mm.datasource.xml", config.getProperty("spring.mm.datasource.xml"));
-        System.setProperty("spring.mm.rice.jta.xml", config.getProperty("spring.mm.rice.jta.xml"));
-        try {
-            String serverPlatform = config.getProperty("server.platform");
-            if (!"JBOSS".equals(serverPlatform)) {
-                // refresh property every 5 minutes
-                Log4jConfigurer.initLogging(config
-                        .getProperty(ContextConstants.LOG4J_SETTINGS_FILE_KEY), 300000);
-            }
-        }
-        catch (Exception e2) {
-            e2.printStackTrace();
-        }
-        ConfigContext.init(config);
-        if (LOG.isDebugEnabled()) {
-            Properties p = ConfigContext.getCurrentContextConfig().getProperties();
-            Enumeration e = p.propertyNames();
-            while (e.hasMoreElements()) {
-                String name = e.nextElement().toString();
-                LOG.debug("name=" + name + " value=" + p.getProperty(name));
-            }
-        }
-        SpringContext.initializeApplicationContext();
+
+				System.setProperty("spring.mm.datasource.xml", "spring-mm-datasource.xml");
+        System.setProperty("spring.mm.rice.jta.xml", "spring-mm-rice-jta.xml");
+
+				LOG.info( "Initializing Web Context" );
+        LOG.info( "Calling KualiInitializeListener.contextInitialized" );
         super.contextInitialized(sce);
-        Formatter.registerFormatter(MMDecimal.class, MMDecimalFormatter.class);
-        LOG.info("Finished web application context initialization");
+        LOG.info( "Completed KualiInitializeListener.contextInitialized" );
+
+        org.kuali.kfs.sys.context.SpringContext.finishInitializationAfterRiceStartup();
+
+
+
+
+//				 PropertyConfigurator.configureAndWatch(settingsFile, reloadMilliseconds);
+//        SimpleConfig config = new SimpleConfig("classpath:" + ContextConstants.CONFIGURATION_LOCATIONS_FILE);
+//        try {
+//            config.parseConfig();
+//        }
+//        catch (IOException e1) {
+//            e1.printStackTrace();
+//        }
+
+//        try {
+//            String serverPlatform = config.getProperty("server.platform");
+//            if (!"JBOSS".equals(serverPlatform)) {
+//                // refresh property every 5 minutes
+//                Log4jConfigurer.initLogging(config
+//                        .getProperty(ContextConstants.LOG4J_SETTINGS_FILE_KEY), 300000);
+//            }
+//        }
+//        catch (Exception e2) {
+//            e2.printStackTrace();
+//        }
+//        ConfigContext.init(config);
+//        if (LOG.isDebugEnabled()) {
+//            Properties p = ConfigContext.getCurrentContextConfig().getProperties();
+//            Enumeration e = p.propertyNames();
+//            while (e.hasMoreElements()) {
+//                String name = e.nextElement().toString();
+//                LOG.debug("name=" + name + " value=" + p.getProperty(name));
+//            }
+//        }
+//        SpringContext.initializeApplicationContext();
+//        super.contextInitialized(sce);
+//        Formatter.registerFormatter(MMDecimal.class, MMDecimalFormatter.class);
+//        LOG.info("Finished web application context initialization");
     }
 
     @Override
